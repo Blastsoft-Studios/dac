2dd67fc76eee1d755dfd20b419bc691cd391e077
Shane <shane@shanerice.me>
2016-07-22 14:19:08 -0700

    Merged for no reason

--- manage.py
+++ manage.py
@@ -8,7 +8,3 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-<<<<<<< HEAD
-=======
-
->>>>>>> da0542e3e26b0e0108fb1218fd0960a2f4e2d9cc
