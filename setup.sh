fbe230213a7d797fa00d0d7707191cc63df47b52
Shane <shane@shanerice.me>
2016-12-23 23:44:26 -0800

    Added help modal and CI

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "Installing requirements via apt-get"
+apt-get -qq update -y
+apt-get -qq install -y python3-pip
+apt-get -qq install -y python3-dev
+apt-get -qq install -y python3-setuptools
+
+echo "Upgrading and installing pip requirements"
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Copying settings.ini.example file to settings.ini"
+cp settings.ini.example settings.ini
+
+echo "----- migrations -----"
+python manage.py makemigrations
+python manage.py migrate
+echo "----- migrations -----"
+
+echo "----- pip freeze -----"
+pip freeze
+echo "----- pip freeze -----"
+
+echo "No tests to run, but Django works, have a nice day..."
